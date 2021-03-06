81a9e20f70dc2d2315918bc8136c7025c78040b2
Matthew Gonzales <gonzalesmattg@gmail.com>
2021-03-06 17:46:58 -0600

    user boards appear upon logon

--- src/javascripts/components/boards.js
+++ src/javascripts/components/boards.js
@@ -1,18 +1,21 @@
 const showBoards = (array) => {
   document.querySelector('#app').innerHTML = `
     <h1>Boards</h1>
-    <div id="boards-container"></div>`;
+    <div id="boards-container" class="row justify-content-center"></div>`;
 
   array.forEach((item) => {
-    document.querySelector('#boards-container').innerHTML += `<div class="card">
-    <h5 class="card-title">${item.title}</h5>
-    <img class="card-img-top" src=${item.image} alt=${item.title} style="height: 400px;">
-    <div class="card-body" style="height: 180px;">
-    <hr>
-    <button class="btn btn-info" data-toggle="modal" data-target="#formModal" id="edit-book-btn--${item.firebaseKey}">Edit Book</button>
-    <button class="btn btn-danger" id="delete-book--${item.firebaseKey}">Delete Book</button>
-    </div>
-  </div>`;
+    document.querySelector('#boards-container').innerHTML += `
+    <div class="col-sm-6">
+      <div class="card">
+        <h5 class="card-title">${item.title}</h5>
+        <img class="card-img-top" src=${item.image} alt=${item.title} style="height: 400px;">
+        <div class="card-body" style="height: 180px;">
+          <hr>
+          <button class="btn btn-info" data-toggle="modal" data-target="#formModal" id="view-board-btn--${item.firebaseKey}">View Board</button>
+          <button class="btn btn-danger" id="delete-board--${item.firebaseKey}">Delete Board</button>
+        </div>
+      </div>
+    </div>`;
   });
 };
 
--- src/javascripts/helpers/auth.js
+++ src/javascripts/helpers/auth.js
@@ -6,6 +6,7 @@ import loginButton from '../components/loginButton';
 import logoutButton from '../components/logoutButton';
 import navBar from '../components/navBar';
 import firebaseConfig from './apiKeys';
+import getBoards from './data/boardData';
 
 const checkLoginStatus = () => {
   firebase.initializeApp(firebaseConfig);
@@ -13,7 +14,7 @@ const checkLoginStatus = () => {
     if (user) {
       // person is logged in do something...
       navBar();
-      showBoards();
+      getBoards(user.uid).then((boards) => showBoards(boards));
       logoutButton();
       // console.warn(user.uid);
     } else {
